fdcf3828671fbcda2194104ba250ed9c59e24979
Daniella Nikov <nutella.daniella@gmail.com>
2025-01-05 16:20:03 +0100

    Set color in vertexshader and pass it to fragmentshader

--- learnopengl/learnopengl/glsl.h
+++ learnopengl/learnopengl/glsl.h
@@ -32,6 +32,23 @@ public:
         "{\n"
         "   FragColor = vec4(0.0f, 0.5f, 0.5f, 1.0f);\n"
         "}\n\0";
+    
+    const char *vertexShaderSourceColored = "#version 330 core\n"
+        "layout (location = 0) in vec3 aPos;\n"
+        "out vec4 vertexColor;\n"
+        "void main()\n"
+        "{\n"
+        "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+        "   vertexColor = vec4(0.5, 0.0, 0.0, 1.0);\n"
+        "}\0";
+    
+    const char *fragmentShaderSourceVertexInput = "#version 330 core\n"
+        "out vec4 FragColor;\n"
+        "in vec4 vertexColor;\n"
+        "void main()\n"
+        "{\n"
+        "   FragColor = vertexColor;\n"
+        "}\n\0";
 };
 
 #endif /* shaders_hpp */
--- learnopengl/learnopengl/main.cpp
+++ learnopengl/learnopengl/main.cpp
@@ -24,7 +24,7 @@ int main() {
 
     // Create and compile shaders
     GLSL glsl;
-    Shader shader(glsl.vertexShaderSource, glsl.fragmentShaderSource1);
+    Shader shader(glsl.vertexShaderSourceColored, glsl.fragmentShaderSourceVertexInput);
     if (!shader.isCompiled()) {
         return -1;
     }
@@ -37,7 +37,7 @@ int main() {
     while (!window.shouldClose()) {
         window.processInput();
 
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClearColor(0.06f, 0.9f, 0.3f, 0.2f);
         glClear(GL_COLOR_BUFFER_BIT);
         
         // Render
